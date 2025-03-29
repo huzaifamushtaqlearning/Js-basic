b46314838f8fdb8ec72e66f77280988cf7c89ca1
huzaifamushtaqofficial <huzaifamushtaqofficial@gmail.com>
2025-03-29 11:54:11 +0500

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-a=1
+ var a=1
 while (a <=10) {
-    document.write(a);
+    document.write(`${a }<br>`);
     a++;
+}
+
+b=1;
+while (b <=10) {
+    document.write("huaifa"+ "<br>");
+    b=b+1;
+}
+
+let yahoobaba = 1;
+let b = yahoobaba;
+
+while (b <= 10) {
+    document.write(b + "<br>");
+    b = b + 1;
 }
