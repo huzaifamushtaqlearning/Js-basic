44e62f1e9ffe6c7a8056aef0e53b7da6d133f3af
huzaifamushtaqofficial <huzaifamushtaqofficial@gmail.com>
2025-03-29 12:00:14 +0500

    first commit

--- loop.js
+++ loop.js
@@ -4,7 +4,7 @@ while (a <=10) {
     a++;
 }
 
-b=1;
+ var b=1;
 while (b <=10) {
     document.write("huaifa"+ "<br>");
     b=b+1;
@@ -17,3 +17,16 @@ while (b <= 10) {
     document.write(b + "<br>");
     b = b + 1;
 }
+//list ki trah
+
+var b = 1;
+document.write("<ul>"); // List start
+
+while (b <= 10) {
+    document.write("<li>huaifa " + b + "</li>");
+    b = b + 1;
+}
+
+document.write("</ul>"); // List end
+
+//table ki trah
